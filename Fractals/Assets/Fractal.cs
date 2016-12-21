aa949e72071f9c6ab28543d3241691c26356313c
Felix Lange <langefx@gmail.com>
2016-12-21 23:39:19 +0100

    scale and move (local)

--- Fractals/Assets/Fractal.cs
+++ Fractals/Assets/Fractal.cs
@@ -8,8 +8,9 @@ public class Fractal : MonoBehaviour {
     public Material material;
 
     public int maxDepth;
-
     private int depth;
+
+    public float childScale;
 
 	// Use this for initialization
 	void Start () {
@@ -24,10 +25,18 @@ public class Fractal : MonoBehaviour {
 
     private void Initialize(Fractal parent)
     {
+        //forwarding everything
         mesh = parent.mesh;
         material = parent.material;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
+        childScale = parent.childScale;
+
+        //parent child relation
+        transform.parent = parent.transform;
+
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
     }
 	
 	// Update is called once per frame
