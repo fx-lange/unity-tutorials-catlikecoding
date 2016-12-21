058de372fe5d74e0f173b56c52c53bc5dd650961
Felix Lange <langefx@gmail.com>
2016-12-21 23:26:47 +0100

    building up the scene + creating childs

---
+++ Fractals/Assets/Fractal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+
+    private int depth;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+
+        if ( depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+        }
+	}
+
+    private void Initialize(Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
