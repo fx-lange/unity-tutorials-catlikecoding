2d8e90c2d563a12b5aa1146e373a82972da43007
Felix Lange <langefx@gmail.com>
2017-01-01 13:12:30 +0100

    setup for performance drop after some seconds

---
+++ Performance/Assets/NucleonSpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawnerScript: MonoBehaviour {
+
+    public float timeBetweenSpawns;
+
+    public float spawnDistance;
+
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+	}
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
