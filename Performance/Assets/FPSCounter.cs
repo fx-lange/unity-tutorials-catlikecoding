60be1d3a1d3061c338ed053423b8c15d85e35adf
Felix Lange <langefx@gmail.com>
2017-01-02 17:49:51 +0100

    fps display added

---
+++ Performance/Assets/FPSCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+    int fps;
+    public int FPS
+    {
+        get { return fps; }
+        private set { fps = value; }
+    }
+
+    //public int FPS { get; private set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+	}
+}
---
+++ Performance/Assets/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour {
+
+    public Text fpsLabel;
+    FPSCounter fpsCounter;
+
+    void Awake()
+    {
+        fpsCounter = GetComponent<FPSCounter>();
+
+    }
+
+    void Update()
+    {
+        fpsLabel.text = Mathf.Clamp(fpsCounter.FPS, 0, 99).ToString();
+    }
+}
