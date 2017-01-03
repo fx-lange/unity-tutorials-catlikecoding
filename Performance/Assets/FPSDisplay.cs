7074814dec8b01da2a5b86ebdf8aca6107cd0f72
Felix Lange <langefx@gmail.com>
2017-01-03 17:05:41 +0100

    colored labels

--- Performance/Assets/FPSDisplay.cs
+++ Performance/Assets/FPSDisplay.cs
@@ -9,6 +9,19 @@ public class FPSDisplay : MonoBehaviour {
     public Text averageFPSLabel, highestFPSLabel, lowestFPSLabel;
     FPSCounter fpsCounter;
 
+    //private so that it won't show up in the global namespace. Make it serializable so that it can be exposed by the Unity editor.
+    [System.Serializable]
+    private struct FPSColor
+    {
+        public Color color;
+        public int minimumFPS;
+    }
+
+    //We'd typically add a public field for that, but we can't do that because the struct itself is private. 
+    //So make the array private as well and give it the SerializeField attribute so Unity exposes it in the editor and saves it.
+    [SerializeField]
+    private FPSColor[] coloring;
+
     void Awake()
     {
         fpsCounter = GetComponent<FPSCounter>();
@@ -17,8 +30,21 @@ public class FPSDisplay : MonoBehaviour {
 
     void Update()
     {
-        averageFPSLabel.text = Mathf.Clamp(fpsCounter.AverageFPS, 0, 99).ToString();
-        lowestFPSLabel.text = Mathf.Clamp(fpsCounter.LowestFPS, 0, 99).ToString();
-        highestFPSLabel.text = Mathf.Clamp(fpsCounter.HighestFPS, 0, 99).ToString();
+        Display(averageFPSLabel,fpsCounter.AverageFPS);
+        Display(lowestFPSLabel, fpsCounter.LowestFPS);
+        Display(highestFPSLabel, fpsCounter.HighestFPS);
+    }
+
+    void Display(Text label, int fps)
+    {
+        label.text = Mathf.Clamp(fps, 0, 99).ToString();
+        for (int i = 0; i < coloring.Length; i++)
+        {
+            if (fps >= coloring[i].minimumFPS)
+            {
+                label.color = coloring[i].color;
+                break;
+            }
+        }
     }
 }
