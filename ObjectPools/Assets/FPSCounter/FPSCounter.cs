f4e292819c80fb821bc6740d034eb9ec01943f66
Felix Lange <langefx@gmail.com>
2017-01-04 18:16:22 +0100

    next tutorial with importet fps counter

---
+++ ObjectPools/Assets/FPSCounter/FPSCounter.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+    int fps;
+    public int AverageFPS
+    {
+        get { return fps; }
+        private set { fps = value; }
+    }
+
+    //in short: public int FPS { get; private set; }
+
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+
+
+    public int frameRange = 60;
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex] = (int)(1f / Time.unscaledDeltaTime);
+        fpsBufferIndex += 1;
+        fpsBufferIndex %= frameRange;
+    }
+
+    void CalculateFPS()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+
+        for (int i = 0; i < frameRange; i++)
+        {
+            int fps = fpsBuffer[i];
+            sum += fps;
+
+            if(fps > highest)
+            {
+                highest = fps;
+            }
+
+            if(fps < lowest)
+            {
+                lowest = fps;
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+
+        UpdateBuffer();
+        CalculateFPS();
+    }
+}
---
+++ ObjectPools/Assets/FPSCounter/FPSDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour {
+
+    public Text averageFPSLabel, highestFPSLabel, lowestFPSLabel;
+    FPSCounter fpsCounter;
+
+    //private so that it won't show up in the global namespace. Make it serializable so that it can be exposed by the Unity editor.
+    [System.Serializable]
+    private struct FPSColor
+    {
+        public Color color;
+        public int minimumFPS;
+    }
+
+    //We'd typically add a public field for that, but we can't do that because the struct itself is private. 
+    //So make the array private as well and give it the SerializeField attribute so Unity exposes it in the editor and saves it.
+    [SerializeField]
+    private FPSColor[] coloring;
+
+    void Awake()
+    {
+        fpsCounter = GetComponent<FPSCounter>();
+
+    }
+
+    void Update()
+    {
+        Display(averageFPSLabel,fpsCounter.AverageFPS);
+        Display(lowestFPSLabel, fpsCounter.LowestFPS);
+        Display(highestFPSLabel, fpsCounter.HighestFPS);
+    }
+
+    void Display(Text label, int fps)
+    {
+        label.text = Mathf.Clamp(fps, 0, 99).ToString();
+        for (int i = 0; i < coloring.Length; i++)
+        {
+            if (fps >= coloring[i].minimumFPS)
+            {
+                label.color = coloring[i].color;
+                break;
+            }
+        }
+    }
+}
