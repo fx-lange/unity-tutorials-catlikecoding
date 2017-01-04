acfa35d8265454d2fc68c7a616e782728abd5f2b
Felix Lange <langefx@gmail.com>
2017-01-04 19:07:13 +0100

    spawning primitives

---
+++ ObjectPools/Assets/StuffSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+
+    public Stuff[] stuffPrefabs;
+
+    public float timeBetweenSpawns;
+    float timeSinceLastSpawn = 0;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if( timeSinceLastSpawn <= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnStuff();
+        }
+    }
+
+    void SpawnStuff()
+    {
+        Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+        Stuff spawn = Instantiate<Stuff>(prefab);
+        spawn.transform.localPosition = transform.localPosition;
+    }
+}
